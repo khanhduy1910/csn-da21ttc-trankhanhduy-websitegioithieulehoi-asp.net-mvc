580f6ccf5f29499f095215db440a52100d2ac6bd
khanhduy1910 <trankhanhduy1409@gmail.com>
2024-01-15 16:46:35 +0700

    cập nhật quyển báo cáo và mã nguồn đồ án

---
+++ WebgioithieudantocKhmer/WebgioithieudantocKhmer/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebgioithieudantocKhmer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        { 
+
+            return View();
+        }
+        public ActionResult Lehoi()
+        {
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+        public ActionResult Lehoi1()
+        {
+
+            return View();
+        }
+        public ActionResult Lehoi2()
+        {
+
+            return View();
+        }
+        public ActionResult Lehoi3()
+        {
+
+            return View();
+        }
+        public ActionResult Lehoi4()
+        {
+
+            return View();
+        }
+        public ActionResult Lehoi5()
+        {
+
+            return View();
+        }
+        public ActionResult Lehoi6()
+        {
+
+            return View();
+        }
+
+    }
+}
